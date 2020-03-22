d64d1b9880704e77741664572433afcbd479dba6
Diego Paiva <diego.paiva13@gmail.com>
2020-03-22 15:02:48 -0300

    Add Automaton.cpp

---
+++ src/Automaton.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file   Automaton.cpp
+ * @author Diego Paiva
+ * @date   11/03/2020
+ *
+ * <Class description here>
+ */
+
+#include "Automaton.hpp"
+
+int Automaton::getStartState()
+{
+  return startState;
+}
