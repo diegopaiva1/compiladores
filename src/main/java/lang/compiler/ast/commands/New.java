2c42ec62bc9591c3a1f7976002dbcda1f520bd60
Diego Paiva <diego.paiva13@gmail.com>
2020-11-09 19:53:31 -0300

    Rename de ast.Instatiation para ast.commands.New

---
+++ src/main/java/lang/compiler/ast/commands/New.java
@@ -0,0 +1,41 @@
+package lang.compiler.ast.commands;
+
+import lang.compiler.visitors.AbstractExpressionEvaluatorVisitor;
+import lang.compiler.ast.AbstractExpression;
+import lang.compiler.ast.types.AbstractType;
+
+public class New extends AbstractCommand {
+  private AbstractType type;
+  private AbstractExpression expr;
+
+  public New(AbstractType type, AbstractExpression expr) {
+    this.type = type;
+    this.expr = expr;
+  }
+
+  public AbstractType getType() {
+    return type;
+  }
+
+  public void setType(AbstractType type) {
+    this.type = type;
+  }
+
+  public AbstractExpression getExpression() {
+    return expr;
+  }
+
+  public void setExpression(AbstractExpression expr) {
+    this.expr = expr;
+  }
+
+  @Override
+  public Object accept(AbstractExpressionEvaluatorVisitor v) {
+    return v.visitNew(this);
+  }
+
+  @Override
+  public String getName() {
+    return "New";
+  }
+}
